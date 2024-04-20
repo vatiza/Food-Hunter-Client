d27f51aca607407abcacc358e99e8f20321291c8
vatiza <rakibur.mac@gmail.com>
2024-04-20 20:43:48 +0600

    navbar logout work

--- src/Components/Layouts/Navbar/Navbar.jsx
+++ src/Components/Layouts/Navbar/Navbar.jsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProviders";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, logOutUser } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logOutUser()
+      .then()
+      .catch((error) => console.log(error));
+  };
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -28,24 +34,37 @@ const Navbar = () => {
             Blog
           </Link>
         </div>
-        {user ? (
-          <div className="btn btn-ghost btn-circle avatar">
-            user?
-            <div className="w-10 rounded-full ">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
-          </div>
-        ) : (
-          <Link
-            to="/login"
-            className="btn btn-ghost text-yellow-500 font-bold text-xl"
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
           >
-            Login
-          </Link>
-        )}
+            {user ? (
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            ) : (
+              <Link
+                to="/login"
+                className="btn btn-ghost text-yellow-500 font-bold text-xl"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a onClick={handleLogout}>Logout</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/Components/Providers/AuthProviders.jsx
+++ src/Components/Providers/AuthProviders.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
 
 export const AuthContext = createContext(null);
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 import app from "../FirebaseConfig/Firebase.config";
 const auth = getAuth(app);
 const AuthProviders = ({ children }) => {
@@ -10,10 +15,21 @@ const AuthProviders = ({ children }) => {
   const createNewUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
-
+  const logOutUser = () => {
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  });
   const authInfo = {
     user,
     createNewUser,
+    logOutUser,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
